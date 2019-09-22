16eeb9c780dfb2bb2ff0b77f5d7006c050290e3a
Natallia <55149770+natallia-js@users.noreply.github.com>
2019-09-23 00:26:24 +0300

    feat: implement a multiply function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let num1 = first.split('').reverse();
+  let num2 = second.split('').reverse();
+
+  let stack = [];
+
+  for (let i = 0; i < num1.length; i++) {
+    for (let j = 0; j < num2.length; j++) {
+      let mult = num1[i] * num2[j];
+      stack[i + j] = (stack[i + j]) ? stack[i + j] + mult : mult;
+    }
+  }
+
+  for (let i = 0; i < stack.length; i++) {
+    let num = stack[i] % 10;
+    let move = Math.floor(stack[i] / 10);
+    stack[i] = num;
+
+    if (stack[i + 1])
+      stack[i + 1] += move;
+    else if (move != 0)
+      stack[i + 1] = move;
+  }
+
+  return stack.reverse().join('');
 }
